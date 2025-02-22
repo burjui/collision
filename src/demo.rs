cea941d725414783dfcc29fd62f27ef2b6a744f0
Artem Borisovskiy <bytefu@gmail.com>
2025-02-22 09:52:08 +0200

    Add small spacing to the brick

--- src/demo.rs
+++ src/demo.rs
@@ -34,7 +34,7 @@ pub fn create_demo(physics: &mut PhysicsEngine) {
 
     let brick = Brick {
         particle_radius: 2.0,
-        particle_spacing: 0.0,
+        particle_spacing: 0.01,
         particle_mass: 0.01,
         ..Brick::new(Vector2::new(400.0, 300.0), Vector2::new(600.0, 300.0))
     };
