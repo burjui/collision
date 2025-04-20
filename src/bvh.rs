e78c82c74aa49d71162b460f28cb9bdf3419cf05
Artem Borisovskiy <bytefu@gmail.com>
2025-04-20 08:35:31 +0200

    Refactor BVH and reuse buffers

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,22 +1,34 @@
 use std::mem::swap;
 
-use itertools::Itertools;
 use rayon::slice::ParallelSliceMut;
 
 use crate::{physics::NormalizedCollisionPair, vector2::Vector2};
 
 #[derive(Default, Clone)]
 pub struct Bvh {
-    items: Vec<Item>,
+    object_indices: Vec<usize>,
     object_aabbs: Vec<AABB>,
     nodes: Vec<Node>,
+    fold_buffer_a: Vec<usize>,
+    fold_buffer_b: Vec<usize>,
 }
 
 impl Bvh {
     pub fn update(&mut self, positions: &[Vector2<f64>], radii: &[f64], morton_codes: &[u32]) {
-        self.items.clear();
+        self.object_indices.clear();
+        self.object_indices.extend(0..positions.len());
+
         self.object_aabbs.clear();
+        self.object_aabbs.reserve(positions.len());
+
         self.nodes.clear();
+        self.nodes.reserve(positions.len() * 2);
+
+        self.fold_buffer_a.clear();
+        self.fold_buffer_a.reserve(positions.len() * 2);
+
+        self.fold_buffer_b.clear();
+        self.fold_buffer_b.reserve(positions.len() * 2);
 
         for object_index in 0..positions.len() {
             let position = positions[object_index];
@@ -25,60 +37,49 @@ impl Bvh {
                 topleft: position - radius,
                 bottomright: position + radius,
             };
-            self.items.push(Item {
-                object_index,
-                morton_code: morton_codes[object_index],
-            });
             self.object_aabbs.push(aabb);
         }
-        self.items.par_sort_unstable_by_key(|item| item.morton_code);
+        self.object_indices.par_sort_unstable_by_key(|&object_index| morton_codes[object_index]);
 
-        for item in &self.items {
+        for &object_index in &self.object_indices {
             self.nodes.push(Node {
-                aabb: self.object_aabbs[item.object_index],
+                aabb: self.object_aabbs[object_index],
                 tag: NodeTag::Leaf,
                 data: NodeData {
-                    leaf_object_index: u32::try_from(item.object_index).unwrap(),
+                    leaf_object_index: u32::try_from(object_index).unwrap(),
                 },
             });
         }
 
-        let mut combine_area = (0..self.nodes.len()).map(|i| NodeId(u32::try_from(i).unwrap())).collect_vec();
-        let mut combine_area_tmp = Vec::with_capacity(combine_area.len().div_ceil(2));
-        let (mut combine_area, mut combine_area_tmp) = (&mut combine_area, &mut combine_area_tmp);
-        while combine_area.len() > 1 {
-            combine_area_tmp.clear();
-            for chunk in combine_area.chunks(2) {
+        let (mut src, mut dst) = (&mut self.fold_buffer_a, &mut self.fold_buffer_b);
+        src.extend(0..self.nodes.len());
+        while src.len() > 1 {
+            dst.clear();
+            for chunk in src.chunks(2) {
                 let node_id = if chunk.len() == 1 {
                     chunk[0]
                 } else {
                     let left = chunk[0];
                     let right = chunk[1];
-                    let left_aabb = self.nodes[usize::try_from(left.0).unwrap()].aabb;
-                    let right_aabb = self.nodes[usize::try_from(right.0).unwrap()].aabb;
+                    let left_aabb = self.nodes[left].aabb;
+                    let right_aabb = self.nodes[right].aabb;
                     let aabb = left_aabb.union(&right_aabb);
-                    let node_id = NodeId(u32::try_from(self.nodes.len()).unwrap());
+                    let node_id = self.nodes.len();
                     self.nodes.push(Node {
                         aabb,
                         tag: NodeTag::Tree,
                         data: NodeData {
-                            tree: Tree { left, right },
+                            tree: Tree {
+                                left: NodeId(left.try_into().unwrap()),
+                                right: NodeId(right.try_into().unwrap()),
+                            },
                         },
                     });
                     node_id
                 };
-                combine_area_tmp.push(node_id);
+                dst.push(node_id);
             }
-            swap(&mut combine_area, &mut combine_area_tmp);
-        }
-
-        self.nodes.reverse();
-        let node_count = u32::try_from(self.nodes.len()).unwrap();
-        for Node { tag, data, .. } in &mut self.nodes {
-            if let NodeTag::Tree = tag {
-                data.tree.left.0 = node_count - unsafe { data.tree.left.0 } - 1;
-                data.tree.right.0 = node_count - unsafe { data.tree.right.0 } - 1;
-            }
+            swap(&mut src, &mut dst);
         }
     }
 
@@ -87,7 +88,7 @@ impl Bvh {
     }
 
     pub fn root(&self) -> NodeId {
-        NodeId(0)
+        NodeId((self.nodes.len() - 1) as u32)
     }
 
     pub fn object_aabbs(&mut self) -> &mut [AABB] {
@@ -96,60 +97,50 @@ impl Bvh {
 
     pub fn find_intersections(
         &self,
-        object_index: usize,
-        positions: &[Vector2<f64>],
-        radii: &[f64],
-        candidates: &mut [NormalizedCollisionPair],
-    ) {
-        if !self.nodes.is_empty() {
-            self.find_intersections_with(object_index, positions, radii, candidates);
-        }
-    }
-
-    fn find_intersections_with(
-        &self,
         object1_index: usize,
         positions: &[Vector2<f64>],
         radii: &[f64],
         candidates: &mut [NormalizedCollisionPair],
     ) {
-        const STACK_SIZE: usize = 32;
-        let mut stack = [NodeId(0); STACK_SIZE];
-        let mut sp = 0;
-        stack[sp] = self.root();
-        sp += 1;
+        if !self.nodes.is_empty() {
+            const STACK_SIZE: usize = 32;
+            let mut stack = [NodeId(0); STACK_SIZE];
+            let mut sp = 0;
+            stack[sp] = self.root();
+            sp += 1;
 
-        let mut candidate_index = 0;
-        while sp > 0 {
-            sp -= 1;
-            let node_id = stack[sp];
-            let object_aabb = self.object_aabbs[object1_index];
-            let node_id = usize::try_from(node_id.0).unwrap();
-            let aabb = self.nodes[node_id].aabb;
-            if object_aabb.intersects(&aabb) {
-                let tag = self.nodes[node_id].tag;
-                match tag {
-                    NodeTag::Leaf => {
-                        let object2_index =
-                            usize::try_from(unsafe { self.nodes[node_id].data.leaf_object_index }).unwrap();
-                        if object2_index != object1_index {
-                            let object1_position = positions[object1_index];
-                            let object2_position = positions[object2_index];
-                            let object1_radius = radii[object1_index];
-                            let object2_radius = radii[object2_index];
-                            let distance_squared = (object1_position - object2_position).magnitude_squared();
-                            let collision_distance = object1_radius + object2_radius;
-                            if distance_squared < collision_distance * collision_distance {
-                                candidates[candidate_index] =
-                                    NormalizedCollisionPair::new(object1_index, object2_index);
-                                candidate_index += 1;
+            let mut candidate_index = 0;
+            while sp > 0 {
+                sp -= 1;
+                let node_id = stack[sp];
+                let object_aabb = self.object_aabbs[object1_index];
+                let node_id = usize::try_from(node_id.0).unwrap();
+                let aabb = self.nodes[node_id].aabb;
+                if object_aabb.intersects(&aabb) {
+                    let tag = self.nodes[node_id].tag;
+                    match tag {
+                        NodeTag::Leaf => {
+                            let object2_index =
+                                usize::try_from(unsafe { self.nodes[node_id].data.leaf_object_index }).unwrap();
+                            if object2_index != object1_index {
+                                let object1_position = positions[object1_index];
+                                let object2_position = positions[object2_index];
+                                let object1_radius = radii[object1_index];
+                                let object2_radius = radii[object2_index];
+                                let distance_squared = (object1_position - object2_position).magnitude_squared();
+                                let collision_distance = object1_radius + object2_radius;
+                                if distance_squared < collision_distance * collision_distance {
+                                    candidates[candidate_index] =
+                                        NormalizedCollisionPair::new(object1_index, object2_index);
+                                    candidate_index += 1;
+                                }
                             }
                         }
-                    }
-                    NodeTag::Tree => {
-                        stack[sp] = unsafe { self.nodes[node_id].data.tree.left };
-                        stack[sp + 1] = unsafe { self.nodes[node_id].data.tree.right };
-                        sp += 2;
+                        NodeTag::Tree => {
+                            stack[sp] = unsafe { self.nodes[node_id].data.tree.left };
+                            stack[sp + 1] = unsafe { self.nodes[node_id].data.tree.right };
+                            sp += 2;
+                        }
                     }
                 }
             }
@@ -170,12 +161,6 @@ fn expand_bits(mut x: u32) -> u32 {
     x = (x | (x << 2)) & 0x33333333;
     x = (x | (x << 1)) & 0x55555555;
     x
-}
-
-#[derive(Clone, Copy)]
-struct Item {
-    object_index: usize,
-    morton_code: u32,
 }
 
 #[repr(C)]
